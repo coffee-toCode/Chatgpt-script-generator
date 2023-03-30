b859d5ebbbd43b5382361c94e91d4d2f710bffc7
coffee-toCode <113392120+coffee-toCode@users.noreply.github.com>
2023-03-30 16:23:06 -0400

    Update python-chatgpt.py

--- python-chatgpt.py
+++ python-chatgpt.py
@@ -32,9 +32,9 @@ request_headers = {
 # Set the request data to include the OpenAI model(which can be changed based on use case), the prompt, and other generation parameters
 request_data = {
    "model": "text-davinci-003",
-   "prompt": f"write python script to {args.prompt}. Provide only the code, no text.",
-   "max_tokens": 100,
-   "temperature": 0.2
+   "prompt": f"write python script to {args.prompt}. Provide full documentation.",
+   "max_tokens": 1000,
+   "temperature": 0.8
    }
    
 
